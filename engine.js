044d96909ed03e80ccea1bf65aa4aa6886c31930
fmfrancie-code <fmfrancie@gmail.com>
2026-09-15 15:24:04 +0200

    Simplify removal of X from engine state array

--- engine.js
+++ engine.js
@@ -18,24 +18,17 @@ export function gestisciUsuraMotore(indiceCasellaSelezionata) {
     let statoKersAggiornato = gameState.kersState;
 
     if (laCasellaContieneGiaUnaX) {
-        // Rimozione della X (da destra verso sinistra)
-        const indiceDaRimuovere = arrayUsureMotoreCorrente.indexOf(indiceCasellaSelezionata);
-        if (indiceDaRimuovere !== -1) {
-            arrayUsureMotoreCorrente.splice(indiceDaRimuovere, 1);
-        }
+        arrayUsureMotoreCorrente.splice(arrayUsureMotoreCorrente.indexOf(indiceCasellaSelezionata), 1);
     } else {
-        // Inserimento della X (da sinistra verso destra)
         arrayUsureMotoreCorrente.push(indiceCasellaSelezionata);
     }
 
     const valoreBaseMotore = gameState.baseValues.engine;
     const puntiAssegnatiSetupMotore = gameState.allocations.engine;
     const totaleCaselleDisponibiliMotore = valoreBaseMotore + puntiAssegnatiSetupMotore;
-
     const tutteLeCaselleMotoreSonoOccupate = (arrayUsureMotoreCorrente.length === totaleCaselleDisponibiliMotore);
 
     if (tutteLeCaselleMotoreSonoOccupate) {
-        // Regola critica Motore: esaurimento totale manda il KERS in fault permanente (damaged)
         statoKersAggiornato = 'damaged';
         messaggioAllertaCritica = "Attenzione: Hai finito i punti motore! Motore in fault e KERS danneggiato.";
     }
