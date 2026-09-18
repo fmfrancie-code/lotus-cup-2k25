f966dbf8e09f8716d7dff93ad3122cfac97fe3b3
fmfrancie-code <fmfrancie@gmail.com>
2026-09-18 12:42:07 +0200

    Update fuel.js

--- fuel.js
+++ fuel.js
@@ -86,7 +86,7 @@ export function gestisciRipristinoBenzinaAiBox(modalitaSceltaBox, numeroCaselleD
 
     if (modalitaSceltaBox === 'pieno') {
         arrayCaselleBenzinaAggiornato = [];
-        descrizioneOperazioneBox = "Rifornimento completato: Pieno di benzina effettuato (tutte le usure rimosse)."[cite: 22];
+        descrizioneOperazioneBox = "Rifornimento completato: Pieno di benzina effettuato (tutte le usure rimosse).";
         stringaMovimentoBox = "+0 MOV";
     } else if (modalitaSceltaBox === 'leggerezza') {
         arrayCaselleBenzinaAggiornato = [];
@@ -99,7 +99,7 @@ export function gestisciRipristinoBenzinaAiBox(modalitaSceltaBox, numeroCaselleD
             stringaMovimentoBox = "+1 MOV";
         }
 
-        descrizioneOperazioneBox = `Strategia di leggerezza applicata: mantenute ${caselleSenzaXRimaste} caselle libere di carburante.`[cite: 22];
+        descrizioneOperazioneBox = `Strategia di leggerezza applicata: mantenute ${caselleSenzaXRimaste} caselle libere di carburante.;
     }
 
     updateGameState({
